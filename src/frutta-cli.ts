33492916def0ec0870932d647c989c97f3baa339
Jess Winter <dev@jesswinter.com>
2025-09-02 17:01:39 +0200

    Remove some useless comments

--- src/frutta-cli.ts
+++ src/frutta-cli.ts
@@ -22,35 +22,6 @@ try {
   process.exit(1);
 }
 
-// const [operation, options] = parseCommandLine(process.argv);
-// let exitCode = 0;
-// if (operation === "error") {
-//   console.log(options);
-//   exitCode = 1;
-// } else if (operation === "showVersion") {
-//   await showVersion();
-// } else if (operation === "startTimer") {
-//   let duration = parseInt(options.duration, 10);
-//   if (options.duration.endsWith("h")) {
-//     duration *= 1000 * 60 * 60;
-//   } else if (options.duration.endsWith("s")) {
-//     duration *= 1000;
-//   } else {
-//     duration *= 1000 * 60;
-//   }
-
-//   if (!(duration > 0)) {
-//     console.error(`Duration must be greater than 0. Got ${options.duration}`);
-//     exitCode = 1;
-//   } else {
-//     exitCode = await startTerminalTimer(duration);
-//   }
-// } else {
-//   showHelp();
-// }
-
-// process.exit(exitCode);
-
 /** @returns the version from package.json */
 async function getVersion(): Promise<string> {
   const scriptDir = path.dirname(url.fileURLToPath(import.meta.url));
